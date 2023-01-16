0ad86e65913e6b3f74d237e5ca328c29b1ae28da
Badr <badrbiyadi@github.com>
2023-01-16 16:28:04 +0100

    Minor change on the routing

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,17 +12,11 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
                     <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.*')">
                         {{ __('Owners') }}
                     </x-nav-link>
                     <x-nav-link :href="route('cars')" :active="request()->routeIs('cars')">
                         {{ __('Cars') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('createCar')" :active="request()->routeIs('createCar')">
-                        {{ __('Create Car') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -72,8 +66,11 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('users.index')" :active="request()->routeIs('users.*')">
+                {{ __('Owners') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('cars')" :active="request()->routeIs('cars')">
+                {{ __('Cars') }}
             </x-responsive-nav-link>
         </div>
 
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('dashboard');
+    return redirect('/cars');
 })->middleware(['auth'])->name('dashboard');
 
 Route::group(['middleware' => 'auth'], function () {
